60a55eb37c243cb1f665f61e2972f1061a9fc428
unknown <it22333520@my.sliit.lk>
2024-04-05 00:07:16 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
+import EventPage from "./pages/EventPage";
+import EventCreationForm from "./pages/EventCreationForm";
 
 export default function App() {
   return (
@@ -8,7 +10,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/log-in" element={<LogInPage />} />
+        <Route path="/Event" element={<EventPage />} />
+        <Route path="/create" element={<EventCreationForm />} />
       </Routes>
     </BrowserRouter>
   );
 }
+   
