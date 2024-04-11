914d30e57075bc7d3a7b86b00a98984b5c7febd4
unknown <it22333520@my.sliit.lk>
2024-04-11 21:17:20 +0530

    Event dash

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,23 @@ import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
 import EventPage from "./pages/EventPage";
 import EventCreationForm from "./pages/EventCreationForm";
+import EventDash from "./pages/EventDash";
+import UpdateEvent from "./pages/UpdateEvent";
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        
+     <Routes>
+
         <Route path="/" element={<HomePage />} />
         <Route path="/log-in" element={<LogInPage />} />
         <Route path="/Event" element={<EventPage />} />
-        <Route path="/create" element={<EventCreationForm />} />
+        <Route path="/create" element={<EventCreationForm/>} />
+        <Route path="/Eventdash" element={<EventDash/>} />
+        <Route path="/Update" element={<UpdateEvent/>} />
+        <Route path="/create" element={<EventCreationForm/>} />
       </Routes>
     </BrowserRouter>
   );
 }
-   
