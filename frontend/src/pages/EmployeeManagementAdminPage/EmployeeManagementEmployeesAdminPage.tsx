37548813fcfe73065541f878dfeece89ed43e831
Sithum Suraweera <sithumsuraweera01@gmail.com>
2024-04-18 18:01:40 +0530

    Graphs and charts add(install the recharts package)

--- frontend/src/pages/EmployeeManagementAdminPage/EmployeeManagementEmployeesAdminPage.tsx
+++ frontend/src/pages/EmployeeManagementAdminPage/EmployeeManagementEmployeesAdminPage.tsx
@@ -22,48 +22,48 @@ export default function EmployeeManagementEmployeesAdminPage() {
 
   return (
     <div className="flex-1">
-      {showEmployeeCreationPopUp && (
-        <EmployeeCreationPopUp setIsVisible={setShowEmployeeCreationPopUp} />
-      )}
+    {showEmployeeCreationPopUp && (
+      <EmployeeCreationPopUp setIsVisible={setShowEmployeeCreationPopUp} />
+    )}
 
-      {!showEmployeeCreationPopUp && (
-        <FilledButton
-          onClick={() => setShowEmployeeCreationPopUp(true)}
-          className="absolute right-4 bottom-6 text-base"
-        >
-          Add New Employee
-        </FilledButton>
-      )}
+    {!showEmployeeCreationPopUp && (
+      <FilledButton
+        onClick={() => setShowEmployeeCreationPopUp(true)}
+        className="absolute right-4 bottom-6 text-base"
+      >
+        Add New Employee
+      </FilledButton>
+    )}
 
-      <div className="p-4 h-screen overflow-auto">
-        <table className="border-2">
-          <thead>
-            <TableHeaderRow>
-              <TableHeaderColumn text="Employee ID" />
-              <TableHeaderColumn text="First Name" />
-              <TableHeaderColumn text="Last Name" />
-              <TableHeaderColumn text="Email" />
-              <TableHeaderColumn text="Address" />
-              <TableHeaderColumn text="Date Joined" />
-              <TableHeaderColumn text="Role" />
-            </TableHeaderRow>
-          </thead>
+    <div className="p-4 h-screen overflow-auto">
+      <table className="border-2">
+        <thead>
+          <TableHeaderRow>
+            <TableHeaderColumn text="Employee ID" />
+            <TableHeaderColumn text="First Name" />
+            <TableHeaderColumn text="Last Name" />
+            <TableHeaderColumn text="Email" />
+            <TableHeaderColumn text="Address" />
+            <TableHeaderColumn text="Date Joined" />
+            <TableHeaderColumn text="Role" />
+          </TableHeaderRow>
+        </thead>
 
-          <tbody>
-            {employees.map((employee, index) => (
-              <TableRow key={employee._id} rowIndex={index}>
-                <TableColumn text={employee._id} />
-                <TableColumn text={employee.firstName} />
-                <TableColumn text={employee.lastName} />
-                <TableColumn text={employee.email} />
-                <TableColumn text={employee.address} />
-                <TableColumn text={employee.dateJoined} />
-                <TableColumn text={employee.role} />
-              </TableRow>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <tbody>
+          {employees.map((employee, index) => (
+            <TableRow key={employee._id} rowIndex={index}>
+              <TableColumn text={employee._id} />
+              <TableColumn text={employee.firstName} />
+              <TableColumn text={employee.lastName} />
+              <TableColumn text={employee.email} />
+              <TableColumn text={employee.address} />
+              <TableColumn text={employee.dateJoined} />
+              <TableColumn text={employee.role} />
+            </TableRow>
+          ))}
+        </tbody>
+      </table>
     </div>
+  </div>
   );
 }
--- frontend/src/pages/SupplierManagementAdminPage/SupplierManagementDashboard.tsx
+++ frontend/src/pages/SupplierManagementAdminPage/SupplierManagementDashboard.tsx
@@ -1,3 +1,64 @@
+import { BarChart, Bar, XAxis, YAxis, Legend, Tooltip, PieChart, Pie } from "recharts";
+
+const barChartData = [
+    { category: "Food", suppliers: 8 },
+    { category: "Medicine", suppliers: 5 },
+    { category: "Toys", suppliers: 7 },
+    { category: "Bathroom Essentials", suppliers: 3 },
+    { category: "Grooming Equipments", suppliers: 4 },
+];
+
+const pieChartData = [
+    { name: "Food", value: 8 },
+    { name: "Medicine", value: 5 },
+    { name: "Toys", value: 7 },
+    { name: "Bathroom Essentials", value: 3 },
+    { name: "Grooming Equipments", value: 4 },
+];
+
 export default function SupplierManagementDashboard() {
-    return <div>SUPPLIER MANAGEMENT DASHBOARD</div>;
+    return (
+        <div className="flex flex-col md:flex-row justify-center items-center">
+            <div className="md:w-1/2">
+                <h1 className="text-2xl font-bold mb-4">The Supplier types</h1>
+                <div className="w-full md:w-3/4 mx-auto">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            dataKey="value"
+                            isAnimationActive={false}
+                            data={pieChartData}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={80}
+                            fill="#E5A700"
+                            label
+                        />
+                        <Tooltip />
+                    </PieChart>
+                </div>
+            </div>
+            <div className="md:w-1/2">
+                <h1 className="text-2xl font-bold mb-4">The Suppliers of Urban Coop</h1>
+                <div className="w-full md:w-3/4 mx-auto">
+                    <BarChart
+                        width={400}
+                        height={400}
+                        data={barChartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="suppliers" fill="#E5A700" />
+                    </BarChart>
+                </div>
+            </div>
+        </div>
+    );
 }
