cfa4d6b3ea0420c85a212a4c56295bf2884f2b47
Sithum Suraweera <sithumsuraweera01@gmail.com>
2024-04-19 16:11:59 +0530

    test - Nothing change

--- frontend/src/pages/InventoryManagementAdminPage/InventoryManagementDashboard.tsx
+++ frontend/src/pages/InventoryManagementAdminPage/InventoryManagementDashboard.tsx
@@ -36,7 +36,6 @@ const renderCustomizedLabel = ({
   innerRadius,
   outerRadius,
   percent,
-  index,
 }: CustomizedLabelProps) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
