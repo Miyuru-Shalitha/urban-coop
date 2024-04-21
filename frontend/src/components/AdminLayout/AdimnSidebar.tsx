1116fc0104debd08cacba887d1d09959c0ae9b4d
Tharusha Nayanajith <155396554+tharusha-nayanajith@users.noreply.github.com>
2024-04-21 15:17:13 +0530

    Reapply "Merge branch 'dev' of https://github.com/Miyuru-Shalitha/urban-coop into dev"
    This reverts commit 7736fb17fdb59762d979631eb4b1d89518ee1a74.

--- frontend/src/components/AdminLayout/AdimnSidebar.tsx
+++ frontend/src/components/AdminLayout/AdimnSidebar.tsx
@@ -50,7 +50,7 @@ export default function AdminSidebar() {
           },
           {
             name: "Report Generation",
-            route: "",
+            route: "/admin/report-generation",
           },
         ],
       },
